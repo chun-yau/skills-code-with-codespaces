9657a07e3aa545f6ff7c8a7bf80bb01a6657008a
chun-yau <94746573+chun-yau@users.noreply.github.com>
2025-10-18 11:48:50 +0100

    Fix export PATH command in postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
 sudo apt-get install sl
-echo "export PATH=\$PATH:U/usr/games" >> ~/.bashrc
-echo "export PATH=\$PATH:U/usr/games" >> ~/.zshrc
+echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/games" >> ~/.zshrc
